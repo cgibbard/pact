79011493438ad3624dfc4ac92d144c884521884e
Joel Burget <joelburget@gmail.com>
2019-02-11 09:09:20 -0800

    Implement TODO.

--- src/Pact/Analyze/Orphans.hs
+++ src/Pact/Analyze/Orphans.hs
@@ -1,10 +1,14 @@
 {-# options_ghc -fno-warn-orphans #-}
 module Pact.Analyze.Orphans where
 
-import           Data.SBV        (Mergeable (symbolicMerge))
+import           Data.SBV        (Mergeable (symbolicMerge), Int64)
+import           Text.PrettyPrint.ANSI.Leijen (Pretty(pretty), text)
 import           Pact.Types.Info
 
 instance Mergeable Info where
   -- Because Info values have no effect on execution we just take the max
   -- (which could possibly have more info)
   symbolicMerge _ _ a b = max a b
+
+instance Pretty Int64 where
+  pretty = text . show
